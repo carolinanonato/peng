c225639a8d356fa1dd889ea3fba2a785b9336b4c
Carolina Nonato <csnonato@gmail.com>
2022-01-31 22:16:03 -0500

    hamburger menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,15 +2,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 import "../styles.css";
 import Hamburger from "./Hamburger";
+import { useState } from "react";
 
 function Nav() {
+  const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const toggleHamburger = () => {
+    setHamburgerOpen(!hamburgerOpen);
+  };
+
   return (
     <>
       <div className="navleft">
         <a className="name-title" href="#home">
           Peng
         </a>
-
         <div className="navright">
           <a className="item" href="#shop">
             Shop
@@ -22,7 +28,30 @@ function Nav() {
             <FontAwesomeIcon icon={faCoffee} />
           </a>
         </div>
-        <Hamburger />
+        <div className="hamburger" onClick={toggleHamburger}>
+          <Hamburger />
+
+          <style jsx>
+            {`
+              @media (max-width: 600px) {
+                .hamburger {
+                  display: fixed;
+                  margin-left: 10px;
+                  z-index: 6;
+                  top: 10px;
+                }
+                .navright {
+                  display: ${hamburgerOpen ? "inline" : "none"};
+                  background-color: rgba(53, 32, 32, 0.849);
+                  height: 300px;
+                  width: 50vw;
+                  margin-top: 50px;
+                  position: fixed;
+                }
+              }
+            `}
+          </style>
+        </div>
       </div>
     </>
   );
